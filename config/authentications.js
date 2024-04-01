e2752f38ab4c4884da4295db6ad168abd17284c9
achyutam <108731061+achyutkadamacl@users.noreply.github.com>
2024-04-01 16:38:38 +0530

    Check user status with auth for security and fix auth token missing error

--- config/authentications.js
+++ config/authentications.js
@@ -1,19 +1,29 @@
 const jwt = require('jsonwebtoken');
-module.exports = (req, res, next) => {
+const models = require('./../models');
+
+module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    res.status(401).json({
-        status: 'fail',
-        message: 'Unauthorized!',
-      });
+
+  if (!authHeader || !authHeader.includes('Bearer')) {
+    return res.status(401).json({
+      status: 'fail',
+      message: 'Unauthorized, invalid token!',
+    });
   }
+
   console.log('header token', authHeader);
   const token = authHeader.split(' ')[1];
   try {
     const user = jwt.verify(token, 'SECRET');
-    console.log('auth--user->', user);
-    req.user = user;
-    next();
+    const user_data = await models.user.findOne({where: {email: user.email}})
+    if (user_data.status == "active") {
+      req.user = user;
+      next();
+    } else {
+      return res.status(401).json({
+        message: `sorry! you are status is ${user_data.status}, Please contact with admin`
+      });
+    }
   } catch (error) {
     res.status(401).json(
       { status: 'fail', message: 'Unauthorized!'}
