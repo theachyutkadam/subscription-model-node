bb9e8ca99cc07f3c0b494ff8b25e512004537225
achyutam <108731061+achyutkadamacl@users.noreply.github.com>
2024-04-08 15:05:40 +0530

    WIP authorization test cases

--- specs/controller_tests/subscription_controller.test.js
+++ specs/controller_tests/subscription_controller.test.js
@@ -47,6 +47,30 @@ describe("Check authentication", () => {
   })
 })
 
+// describe("Check role base access/ authorization", () => {
+//   it('access denied for get subscriptions', async () => {
+//     const res = await request(app).get('/api/subscriptions').set('Authorization', `Bearer ${token}`)
+//     expect(res.status).toEqual(401)
+//     expect(res.body.message).toEqual("sorry! You don't have a access for this action")
+//   })
+//   it('access denied for get subscription', async () => {
+//     const res = await request(app).get('/api/subscriptions/1').set('Authorization', `Bearer ${token}`)
+//     expect(res.status).toEqual(401)
+//   })
+//   it('access denied for create subscription', async () => {
+//     const res = await request(app).post('/api/subscriptions').send(newSubscriptionPayload).set('Authorization', `Bearer ${token}`)
+//     expect(res.status).toEqual(401)
+//   })
+//   it('access denied for update subscription', async () => {
+//     const res = await request(app).put('/api/subscriptions/11').send(updateSubscriptionPayload).set('Authorization', `Bearer ${token}`)
+//     expect(res.status).toEqual(401)
+//   })
+//   it('access denied for delete subscription', async () => {
+//     const res = await request(app).delete('/api/subscriptions/11').set('Authorization', `Bearer ${token}`)
+//     expect(res.status).toEqual(401)
+//   })
+// })
+
 describe("Success Subscriptions APIs", () => {
   it('should show all subscriptions', async () => {
     const res = await request(app).get('/api/subscriptions').set('Authorization',  `Bearer ${token}`)
